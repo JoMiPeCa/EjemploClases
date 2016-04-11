0e78b6d0b512bb71b564ca386bb61ff30bf72e42
cetecom <cetecom@LC08L1313>
2016-04-11 20:57:04 -0300

    Avance lateral

--- EjemploClases/ingUsuario.php
+++ EjemploClases/ingUsuario.php
@@ -1,9 +1,13 @@
 <?php
 require 'Usuario.php';
 
-$oUsr = new Usuario($rut, $nom, $apell);
+$oUsr = new Usuario($_POST["rut"], $_POST["nombre"], $_POST["apellido"]);
 
+//var_dump($oUsr);
 
-
-
+?><BR>Rut: <?= $oUsr->getSRut();
+?><br>Nombre: <?= $oUsr->getSNombre();
+?></br>Apellido: <?= $oUsr->getSApellido();
 ?>
+
+
